692080d455b0452f6a6b850d2f3539c5b3e8f5d5
Kwon KiHoon <inyourcareallen@gmail.com>
2023-07-09 08:34:20 +0900

    nav 기본 틀 완성

--- components/root/nav/SubLinks.tsx
+++ components/root/nav/SubLinks.tsx
@@ -11,18 +11,17 @@ function SubLinks({ subMapKey }: { subMapKey: string }) {
     { href: "/intro/sangsang", tagName: "상상코칭" },
     { href: "/intro/howcoding", tagName: "하우코딩" },
     { href: "/intro/goodo", tagName: "공부구도" },
-    { href: "/intro/wawa", tagName: "학원" },
   ]);
   subMap.set(subMapKeySelector.fun, [
     { href: "/fun/food_roulette", tagName: "점심추천" },
   ]);
   return (
-    <nav className="absolute">
+    <nav className={`absolute min-w-[500px] grid grid-cols-8 gap-x-8`}>
       {subMap.get(subMapKey)?.map((sublink) => (
         <Link
           key={sublink.tagName}
           href={sublink.href}
-          className="m-0 text-xs sm:text-lg md:text-lg lg:text-lg xl:text-lg 2xl:text-lg"
+          className="m-0 text-xs sm:text-base md:text-base lg:text-base xl:text-base 2xl:text-base whitespace-nowrap"
         >
           {sublink.tagName}
         </Link>
