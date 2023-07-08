6f93148d9f54f24edf420e21e40c50a0194a6dd2
Kwon KiHoon <inyourcareallen@gmail.com>
2023-07-08 15:40:45 +0900

    footer 비즈니스 정보 데이터베이스화

---
+++ app/api/businessinfo/list/route.ts
@@ -0,0 +1,76 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient, ContactInfo } from "@prisma/client";
+import { NextResponse } from "next/server";
+const prisma = new PrismaClient();
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  console.log("businessinfo list api - request body::", body);
+  const { page, limit, lastId, conditions } = body;
+  console.log("businessinfo list api - page,limit,conditions", page, limit, conditions);
+
+  const whereConditions = {
+    ...conditions,
+  };
+  const businessInfos = await prisma.businessInfo.findMany({
+
+    ...(page && limit && { skip: page * limit }),
+    ...(limit && { take: limit }),
+    ...(lastId && { skip: 1, cursor: { id: lastId } }),
+    select: {
+      id: true,
+      name: true,
+      kind: true,
+      phone: true,
+      // password:true,
+      address: true,
+      address2: true,
+      description: true,
+      // role: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+    where: whereConditions,
+    // where: {
+    //     creator: {
+    //         email: 'admin@sotong.co.kr'
+    //     }
+    // },
+    orderBy: [
+      {
+        createdAt: "desc",
+      },
+      {
+        name: "asc",
+      },
+    ],
+    // include: {
+    //     creator: {
+    //         select: {
+    //             name: true, email: true, image: true, role: true
+    //         }
+    //     },
+    //     modifier: {
+    //         select: {
+    //             name: true, email: true, image: true, role: true
+    //         }
+    //     }
+    // }
+  });
+  console.debug("businessinfo list api result", businessInfos.length);
+  const total = await prisma.businessInfo.count({
+    where: whereConditions,
+    // where: {
+    //     creator: {
+    //         email: 'admin@sotong.co.kr'
+    //     }
+    // }
+  });
+  const pages =
+    total === 0 ? 1 : Math.floor(total / limit) + (total % limit === 0 ? 0 : 1);
+  // res.status(200).json({ contactinfos, pages });
+
+  // console.log(contactinfo);
+  await prisma.$disconnect();
+  return NextResponse.json({ businessInfos, pages });
+}
--- components/root/Footer.tsx
+++ components/root/Footer.tsx
@@ -1,23 +1,31 @@
+import { PrismaClient } from "@prisma/client";
 import React from "react";
 
-function Footer() {
+async function Footer() {
+  const prisma = new PrismaClient();
+  const businessInfo = await prisma.businessInfo.findFirst({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
   return (
     <footer>
+      {/* {businessInfo?.bossName} */}
       <div className="w-full flex justify-center items-center bg-gray-100 h-[50px] border-y-2 border-gray-200 text-sm mt-[100px]">
-        <p>{`COPYRIGHT (C) 과외4U. ALL RIGHTS RESERVED.`}</p>
+        <p>{`COPYRIGHT (C) ${businessInfo?.name}. ALL RIGHTS RESERVED.`}</p>
       </div>
       <div className="w-full flex flex-col py-[20px] prose mx-auto text-sm ">
         <p className="my-1">
-          <strong>업체명</strong> 과외4U | <strong>대표자</strong> 권기훈 |{" "}
-          <strong>사업자등록번호</strong> 603-21-65656
+          <strong>업체명</strong> {businessInfo?.name} | <strong>대표자</strong>{" "}
+          {businessInfo?.bossName} | <strong>사업자등록번호</strong>{" "}
+          {businessInfo?.buisinessRegistNumber}
         </p>
         <p className="my-1">
-          <strong>대표전화</strong> 5961-5807 | (평일, 주말) 08:00 ~ 22:00 |
-          서울특별시 동대문구 왕산로 25
+          <strong>대표전화</strong> {businessInfo?.phone} |{" "}
+          {businessInfo?.worktime} |{businessInfo?.address}
         </p>
         <p className="my-1">
-          <strong>제휴사</strong> 상상코칭 | e-상상코칭 | 와와학습코칭센터 |
-          Power English | 쏠루트 유학센터
+          <strong>제휴사</strong> {businessInfo?.partners}
         </p>
       </div>
     </footer>
