efdd573deedb8c42432285a8df9b13a518cf85dd
Kwon KiHoon <inyourcareallen@gmail.com>
2023-07-08 17:41:19 +0900

    nav 글자 변경

--- components/root/nav/Nav.tsx
+++ components/root/nav/Nav.tsx
@@ -58,7 +58,7 @@ function Nav() {
           href={"/fun/food_roulette"}
           className="m-0 text-xs sm:text-lg md:text-lg lg:text-lg xl:text-lg 2xl:text-lg"
         >
-          점심룰렛
+          점심추천
         </Link>
       </nav>
       <div className="hidden sm:hidden md:hidden lg:flex xl:flex 2xl:flex flex-row justify-between items-center">
