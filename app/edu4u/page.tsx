87c0513c095760ae064007529da09d71851fbe3d
Kwon KiHoon <inyourcareallen@gmail.com>
2023-07-09 11:43:30 +0900

    메인 이미지 등록

--- app/edu4u/page.tsx
+++ app/edu4u/page.tsx
@@ -6,6 +6,12 @@ export default function Home() {
       <header className="w-full flex flex-col py-5 bg-[rgba(35,46,82,0)]">
         <Header />
       </header>
+      <div className="flex justify-center itmes-center">
+        <div className="w-full w-[60vw] h-[300px] bg-[url('/images/edu4u/main.png')] bg-no-repeat bg-center bg-cover"></div>
+        <div>
+          
+        </div>
+      </div>
     </main>
   );
 }
