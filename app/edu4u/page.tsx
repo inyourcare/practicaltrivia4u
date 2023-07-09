a98a8bb68ea04cc3f75f04f3398ccc5f41c79fe6
Kwon KiHoon <inyourcareallen@gmail.com>
2023-07-09 12:12:39 +0900

    4개 이미지 추가

--- app/edu4u/page.tsx
+++ app/edu4u/page.tsx
@@ -6,10 +6,15 @@ export default function Home() {
       <header className="w-full flex flex-col py-5 bg-[rgba(35,46,82,0)]">
         <Header />
       </header>
-      <div className="flex justify-center itmes-center">
-        <div className="w-full w-[60vw] h-[300px] bg-[url('/images/edu4u/main.png')] bg-no-repeat bg-center bg-cover"></div>
-        <div>
-          
+      <div className="w-full flex flex-col justify-center items-center ">
+        <div className="w-[60vw] h-[300px] bg-[url('/images/edu4u/main.png')] bg-no-repeat bg-center bg-cover"></div>
+        <div className="w-[80vw]">
+          <div className="grid grid-cols-2 lg:grid-cols-4">
+            <div className="py-[70%]  bg-[url('/images/edu4u/korean.png')] bg-no-repeat bg-center bg-contain hover:cursor-pointer"></div>
+            <div className="py-[70%]  bg-[url('/images/edu4u/math.png')] bg-no-repeat bg-center bg-contain hover:cursor-pointer"></div>
+            <div className=" py-[70%]  bg-[url('/images/edu4u/english.png')] bg-no-repeat bg-center bg-contain hover:cursor-pointer"></div>
+            <div className="py-[70%] bg-[url('/images/edu4u/enrichment.png')] bg-no-repeat bg-center bg-contain hover:cursor-pointer"></div>
+          </div>
         </div>
       </div>
     </main>
