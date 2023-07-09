79f81404afa27f8b154ba59f86c8d616ce6c0a22
Kwon KiHoon <inyourcareallen@gmail.com>
2023-07-09 20:18:57 +0900

    과외4u 상세페이지 작업

---
+++ app/edu4u/math/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="w-full flex flex-col justify-center items-center ">
+        <div className="w-[60vw] pt-[250%] bg-[url('/images/edu4u/detail/math_detail.png')] bg-no-repeat bg-center bg-contain"></div>
+      </div>
+      <div className="w-full flex flex-col justify-center items-center ">
+        <div className="w-[60vw] pt-[50%] bg-[url('/images/edu4u/why4u.png')] bg-no-repeat bg-center bg-contain"></div>
+      </div>
+    </main>
+  );
+}
--- app/edu4u/page.tsx
+++ app/edu4u/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
@@ -5,14 +7,22 @@ export default function Home() {
         <div className="w-[60vw] h-[300px] bg-[url('/images/edu4u/main.png')] bg-no-repeat bg-center bg-cover"></div>
         <div className="w-[80vw]">
           <div className="grid grid-cols-2 lg:grid-cols-4">
-            <div className="py-[70%]  bg-[url('/images/edu4u/korean.png')] bg-no-repeat bg-center bg-contain hover:cursor-pointer"></div>
-            <div className="py-[70%]  bg-[url('/images/edu4u/math.png')] bg-no-repeat bg-center bg-contain hover:cursor-pointer"></div>
-            <div className=" py-[70%]  bg-[url('/images/edu4u/english.png')] bg-no-repeat bg-center bg-contain hover:cursor-pointer"></div>
-            <div className="py-[70%] bg-[url('/images/edu4u/enrichment.png')] bg-no-repeat bg-center bg-contain hover:cursor-pointer"></div>
+            <Link href={"/edu4u/korean"}>
+              <div className="py-[70%]  bg-[url('/images/edu4u/korean.png')] bg-no-repeat bg-center bg-contain hover:cursor-pointer"></div>
+            </Link>
+            <Link href={"/edu4u/math"}>
+              <div className="py-[70%]  bg-[url('/images/edu4u/math.png')] bg-no-repeat bg-center bg-contain hover:cursor-pointer"></div>
+            </Link>
+            <Link href={"/edu4u/english"}>
+              <div className=" py-[70%]  bg-[url('/images/edu4u/english.png')] bg-no-repeat bg-center bg-contain hover:cursor-pointer"></div>
+            </Link>
+            <Link href={"/edu4u/enrichment"}>
+              <div className="py-[70%] bg-[url('/images/edu4u/enrichment.png')] bg-no-repeat bg-center bg-contain hover:cursor-pointer"></div>
+            </Link>
           </div>
         </div>
-        <div className="relative w-[60vw] ">
-          <div className="w-full py-[60%] bg-[url('/images/edu4u/why4u.png')] bg-no-repeat bg-center bg-contain"></div>
+        <div className="w-full flex justify-center items-center ">
+          <div className="w-[60vw] pt-[50%] bg-[url('/images/edu4u/why4u.png')] bg-no-repeat bg-center bg-contain"></div>
         </div>
       </div>
     </main>
