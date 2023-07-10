3b6867434d8b33dbe6337202a3700aa23c5d6392
Kwon KiHoon <inyourcareallen@gmail.com>
2023-07-10 20:06:41 +0900

    모바일 아래 버튼 완성

--- app/edu4u/page.tsx
+++ app/edu4u/page.tsx
@@ -161,6 +161,19 @@ export default function Home() {
             상담 문의하기
           </div>
         </div>
+        <div className={`block md:hidden w-full cursor-pointer ${askDialogOpen && 'origin-bottom scale-y-0'}`}>
+          <div className={`fixed bottom-0 w-full h-[70px] bg-blue-100 flex flex-row `}>
+            <div className="w-1/2 p-10 text-center flex justify-center items-center bg-green-300 font-black text-2xl text-white cursor-pointer rounded-lg">
+              <a href="tel:27197294">전화상담</a>
+            </div>
+            <div
+              className="w-1/2 p-10 text-center flex justify-center items-center bg-rose-500 font-black text-2xl text-white cursor-pointer rounded-lg"
+              onClick={() => setAskDialogOpen(!askDialogOpen)}
+            >
+              상담문의
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
