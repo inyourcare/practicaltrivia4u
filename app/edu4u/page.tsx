919e152b7f2632c75eed04514b177e65cab20840
Kwon KiHoon <inyourcareallen@gmail.com>
2023-07-10 17:19:13 +0900

    상담하기 버튼 생성

--- app/edu4u/page.tsx
+++ app/edu4u/page.tsx
@@ -145,6 +145,9 @@ export default function Home() {
             </p>
           </div>
         </div>
+        <div className="w-[80vw] h-[100px] p-10 text-center flex justify-center items-center bg-slate-500 mt-10 font-black text-5xl text-white cursor-pointer">
+          상담 문의하기
+        </div>
       </div>
     </main>
   );
