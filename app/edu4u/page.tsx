400dbaf8ac27408a172922724796b2964871e06a
Kwon KiHoon <inyourcareallen@gmail.com>
2023-07-11 18:30:04 +0900

    메인이미지 60->80vw

--- app/edu4u/page.tsx
+++ app/edu4u/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main>
       <div className="w-full flex flex-col justify-center items-center ">
         
-        <div className="w-[60vw] h-[300px] bg-[url('/images/edu4u/main.png')] bg-no-repeat bg-center bg-cover"></div>
+        <div className="w-[80vw] h-[300px] bg-[url('/images/edu4u/main.png')] bg-no-repeat bg-center bg-cover"></div>
         <div className="w-[80vw]">
           <div className="grid grid-cols-2 lg:grid-cols-4">
             <Link href={"/edu4u/korean"}>
