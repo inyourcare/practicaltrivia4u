5f438448fad386ff13fb38b084d6e2c478bd3191
Kwon KiHoon <inyourcareallen@gmail.com>
2023-07-12 21:41:02 +0900

    세컨메뉴 디자인 수정

--- components/root/nav/Nav.tsx
+++ components/root/nav/Nav.tsx
@@ -7,7 +7,7 @@ import { linkMap, rootLinks } from "./links";
 function Nav() {
   const [isMouseOver, setIsMouseOver] = useState(false);
   const [subMapKey, setSubMapKey] = useState("");
-  const navLinkClassName = `m-0 text-xs whitespace-nowrap sm:text-lg md:text-lg lg:text-lg xl:text-lg 2xl:text-lg`
+  const navLinkClassName = `m-0 text-xs whitespace-nowrap sm:text-lg md:text-lg lg:text-lg xl:text-lg 2xl:text-lg`;
   return (
     <div
       className="relative"
@@ -16,52 +16,68 @@ function Nav() {
     >
       <nav className="gap-3 sm:gap-5 md:gap-10 lg:gap-10 xl:gap-10 2xl:gap-10 inline-flex justify-center sm:justify-center md:justify-right mt-5 sm:mt-5 md:mt-0 lg:mt-0 xl:mt-0 2xl:mt-0  items-start text-left font-medium">
         <div className="relative">
-          <Link
-            href={linkMap.get(rootLinks.edu4u)?.href as string}
-            className={navLinkClassName}
+          <div
             onMouseOver={() => setSubMapKey(rootLinks.edu4u)}
+            className="min-h-[40px]"
           >
-            {linkMap.get(rootLinks.edu4u)?.tagName as string}
-          </Link>
+            <Link
+              href={linkMap.get(rootLinks.edu4u)?.href as string}
+              className={navLinkClassName}
+            >
+              {linkMap.get(rootLinks.edu4u)?.tagName as string}
+            </Link>
+          </div>
           <SubLinks
             subMapKey={rootLinks.edu4u}
             isOpen={isMouseOver && subMapKey === rootLinks.edu4u}
           />
         </div>
         <div className="relative">
-          <Link
-            href={linkMap.get(rootLinks.partners)?.href as string}
-            className={navLinkClassName}
+          <div
             onMouseOver={() => setSubMapKey(rootLinks.partners)}
+            className="min-h-[40px]"
           >
-            {linkMap.get(rootLinks.partners)?.tagName as string}
-          </Link>
+            <Link
+              href={linkMap.get(rootLinks.partners)?.href as string}
+              className={navLinkClassName}
+            >
+              {linkMap.get(rootLinks.partners)?.tagName as string}
+            </Link>
+          </div>
           <SubLinks
             subMapKey={rootLinks.partners}
             isOpen={isMouseOver && subMapKey === rootLinks.partners}
           />
         </div>
         <div className="relative">
-          <Link
-            href={linkMap.get(rootLinks.post)?.href as string}
-            className={navLinkClassName}
+          <div
             onMouseOver={() => setSubMapKey(rootLinks.post)}
+            className="min-h-[40px]"
           >
-            {linkMap.get(rootLinks.post)?.tagName as string}
-          </Link>
+            <Link
+              href={linkMap.get(rootLinks.post)?.href as string}
+              className={navLinkClassName}
+            >
+              {linkMap.get(rootLinks.post)?.tagName as string}
+            </Link>
+          </div>
           <SubLinks
             subMapKey={rootLinks.post}
             isOpen={isMouseOver && subMapKey === rootLinks.post}
           />
         </div>
         <div className="relative">
-          <Link
-            href={linkMap.get(rootLinks.fun)?.href as string}
-            className={navLinkClassName}
+          <div
             onMouseOver={() => setSubMapKey(rootLinks.fun)}
+            className="min-h-[40px]"
           >
-            {linkMap.get(rootLinks.fun)?.tagName as string}
-          </Link>
+            <Link
+              href={linkMap.get(rootLinks.fun)?.href as string}
+              className={navLinkClassName}
+            >
+              {linkMap.get(rootLinks.fun)?.tagName as string}
+            </Link>
+          </div>
           <SubLinks
             subMapKey={rootLinks.fun}
             isOpen={isMouseOver && subMapKey === rootLinks.fun}
--- components/root/nav/SubLinks.tsx
+++ components/root/nav/SubLinks.tsx
@@ -12,7 +12,7 @@ function SubLinks({
     <>
       {subMap.get(subMapKey) && (
         <nav
-          className={`absolute flex flex-wrap bg-gray-200 p-3 transition ease-in-out delay-150 origin-top ${
+          className={`absolute -left-3 flex flex-wrap bg-gray-100 transition ease-in-out delay-150 origin-top min-w-[120px] ${
             isOpen ? "scale-y-100" : "scale-y-0"
           }`}
         >
@@ -20,7 +20,7 @@ function SubLinks({
             <Link
               key={sublink.tagName}
               href={sublink.href}
-              className="m-0 text-xs sm:text-base md:text-base lg:text-base xl:text-base 2xl:text-base whitespace-nowrap"
+              className="m-0 text-xs sm:text-base md:text-base lg:text-base xl:text-base 2xl:text-base whitespace-nowrap min-h-[50px] hover:border hover:bg-gray-200 border-gray-400 w-full flex justify-center items-center"
             >
               {sublink.tagName}
             </Link>
