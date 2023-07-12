8b601df030373498e63931573efbbd8c559900c9
Kwon KiHoon <inyourcareallen@gmail.com>
2023-07-12 21:25:47 +0900

    파워잉글리시 페이지 추가

---
+++ app/partners/power/page.tsx
@@ -0,0 +1,212 @@
+import YoutubePopup from "@/components/util/youtube/popup/YoutubePopup";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center w-full py-10">
+          <Image
+            width={1600}
+            height={831}
+            src={`/images/partners/power/1.png`}
+            alt=""
+            // className="cursor-pointer"
+          />
+        </div>
+      </div>
+
+      <div className="h-[200px] md:h-[400px] flex justify-center items-center">
+        <Image
+          width={2000}
+          height={947}
+          src={`/images/partners/power/2.png`}
+          alt=""
+        />
+      </div>
+
+      <div className="flex justify-center items-center py-10">
+        <div className="flex justify-center items-center w-full ">
+          <Image
+            width={1400}
+            height={1200}
+            src={`/images/partners/power/3.png`}
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center w-full">
+          <Image
+            width={1085}
+            height={1409}
+            src={`/images/partners/power/4.png`}
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center w-full py-10">
+          <Image
+            width={1600}
+            height={705}
+            src={`/images/partners/power/5.png`}
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center w-full bg-gray-300 py-10">
+          <Image
+            width={1119}
+            height={585}
+            src={`/images/partners/power/6.png`}
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center w-full bg-gray-300 py-10">
+          <Image
+            width={758}
+            height={60}
+            src={`/images/partners/power/6-1.png`}
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-4">
+        <div className="flex justify-center items-center">
+          <div className="flex justify-center items-center w-full bg-gray-300 py-10">
+            <Image
+              width={400}
+              height={300}
+              src={`/images/partners/power/6-2.jpg`}
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="flex justify-center items-center">
+          <div className="flex justify-center items-center w-full bg-gray-300 py-10">
+            <Image
+              width={400}
+              height={300}
+              src={`/images/partners/power/6-3.jpg`}
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="flex justify-center items-center">
+          <div className="flex justify-center items-center w-full bg-gray-300 py-10">
+            <Image
+              width={400}
+              height={300}
+              src={`/images/partners/power/6-4.jpg`}
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="flex justify-center items-center">
+          <div className="flex justify-center items-center w-full bg-gray-300 py-10">
+            <Image
+              width={400}
+              height={300}
+              src={`/images/partners/power/6-5.jpg`}
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center w-full py-10">
+          <Image
+            width={1600}
+            height={2100}
+            src={`/images/partners/power/7.png`}
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center w-full py-10">
+          <YoutubePopup videoId="cMvJhQ5jZkU">
+            <Image
+              width={921}
+              height={174}
+              src={`/images/partners/power/8-cMvJhQ5jZkU.png`}
+              alt=""
+            />
+          </YoutubePopup>
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center w-full bg-gray-300 py-10">
+          <Image
+            width={1043}
+            height={211}
+            src={`/images/partners/power/9.png`}
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center w-full py-10">
+          <Image
+            width={1339}
+            height={540}
+            src={`/images/partners/power/10.png`}
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center w-full py-10">
+          <Image
+            width={1339}
+            height={540}
+            src={`/images/partners/power/11.png`}
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center w-full bg-gray-300 py-10">
+          <Image
+            width={877}
+            height={760}
+            src={`/images/partners/power/12.png`}
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center w-full py-10">
+          <Image
+            width={1084}
+            height={207}
+            src={`/images/partners/power/13.png`}
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center w-full py-10">
+          <Image
+            width={1011}
+            height={498}
+            src={`/images/partners/power/13-1.png`}
+            alt=""
+          />
+        </div>
+      </div>
+    </main>
+  );
+}
--- components/root/nav/links.ts
+++ components/root/nav/links.ts
@@ -30,6 +30,7 @@ subMap.set(rootLinks.partners, [
   { href: "/partners/goodo", tagName: "공부구도" },
   { href: "/partners/wawa", tagName: "와와" },
   { href: "/partners/mindfulness", tagName: "마음키움" },
+  { href: "/partners/power", tagName: "파워잉글리쉬" },
 ]);
 subMap.set(rootLinks.post, [{ href: "/post", tagName: "POST" }]);
 subMap.set(rootLinks.fun, [
