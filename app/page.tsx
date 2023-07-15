0952fcbce8ce45ddb225768a8f4f3777344e4727
Kwon KiHoon <inyourcareallen@gmail.com>
2023-07-15 10:49:37 +0900

    next redirect 가 next 페이지를 캐싱해서 미리 보여주는게 문제같아서 useeffect 사용하도록 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
+"use client";
 import Header from "@/components/root/Header";
-import { redirect } from 'next/navigation'
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  redirect('/edu4u')
+  // redirect('/edu4u')
+  useEffect(() => {
+    const link = document.getElementById(
+      "main-page-link-id"
+    ) as HTMLAnchorElement;
+    link.click();
+  }, []);
   return (
     <main>
       <header className="w-full flex flex-col py-5 bg-[rgba(35,46,82,0)]">
         <Header />
       </header>
+      <Link id="main-page-link-id" href="/edu4u"></Link>
     </main>
   );
 }
