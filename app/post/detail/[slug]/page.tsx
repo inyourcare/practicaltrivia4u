aae84e389da1de5176733f631b4234d4093a9fae
Kwon KiHoon <inyourcareallen@gmail.com>
2023-07-17 02:37:24 +0900

    지난번에 안됐었는데? fetch 개선

--- app/post/detail/[slug]/page.tsx
+++ app/post/detail/[slug]/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+// "use client";
 import Image from "next/image";
 // import { Metadata } from "next";
 // import Markdown from "markdown-to-jsx";
@@ -7,6 +7,8 @@ import Image from "next/image";
 import parse from "html-react-parser";
 import { useEffect, useState } from "react";
 import Spinner from "@/components/spinner/Spinner";
+import Head from "next/head";
+import { Metadata } from "next";
 
 export interface PostMetadata {
   id: string;
@@ -23,13 +25,13 @@ export interface PostMetadata {
 
 const getData = async (id: string) => {
   const res = await fetch(
-    // `${process.env.NEXT_PUBLIC_API_HOST}/api/post/${id}`,
-    `/api/post/${id}`,
+    `${process.env.NEXT_PUBLIC_API_HOST}/api/post/${id}`,
+    // `/api/post/${id}`,
     {
       method: "GET",
       headers: { "Content-Type": "application/json" },
       // cache: process.env.NODE_ENV !== "development" && "default" || "no-cache"
-      // cache: "no-cache",
+      cache: "no-cache",
       next: { revalidate: 10 },
     }
   );
@@ -37,39 +39,39 @@ const getData = async (id: string) => {
   return res.json();
 };
 
-// export const generateMetadata = async ({
-//   params,
-// }: {
-//   params: { slug: string };
-// }): Promise<Metadata> => {
-//   const slug = params.slug;
-//   const { post } = await getData(slug);
-//   return {
-//     title: post.title,
-//     description: post.description,
-//   };
-// };
-
-export default function PostHome({ params }: { params: { slug: string } }) {
+export const generateMetadata = async ({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata> => {
   const slug = params.slug;
-  // const { post } = await getData(slug);
+  const { post } = await getData(slug);
+  return {
+    title: post.title,
+    description: post.description,
+  };
+};
+
+export default async function PostHome({ params }: { params: { slug: string } }) {
+  const slug = params.slug;
+  const { post } = await getData(slug);
   // const matterResult = matter(post.contents);
-  const [post, setPost] = useState(null as unknown as PostMetadata);
-  const [isLoading, setIsLoading] = useState(false);
-  useEffect(() => {
-    setIsLoading(true);
-    getData(slug)
-      .then((data) => {
-        console.log(data);
-        // const [posts, pages] = data;
-        if (data.post) {
-          setPost(data.post);
-        }
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, [slug]);
+  // const [post, setPost] = useState(null as unknown as PostMetadata);
+  // const [isLoading, setIsLoading] = useState(false);
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   getData(slug)
+  //     .then((data) => {
+  //       console.log(data);
+  //       // const [posts, pages] = data;
+  //       if (data.post) {
+  //         setPost(data.post);
+  //       }
+  //     })
+  //     .finally(() => {
+  //       setIsLoading(false);
+  //     });
+  // }, [slug]);
   return (
     <>
       {/* <PostHeader
@@ -80,9 +82,13 @@ export default function PostHome({ params }: { params: { slug: string } }) {
         description={post.data.description}
       /> */}
 
-      {isLoading && <Spinner />}
-      {(post && (
+      {/* {isLoading && <Spinner />} */}
+      {post && (
         <>
+          <Head>
+            <title>{post.title}</title>
+            <meta name="description" content={`${post.description}`} />
+          </Head>
           <div className="my-10 mx-auto w-full max-h-[500px] px-20 flex justify-center items-center">
             <Image
               width="500"
@@ -98,13 +104,13 @@ export default function PostHome({ params }: { params: { slug: string } }) {
             <h1 className="font-black text-2xl">{post.title}</h1>
           </div>
           <div className="my-12 prose prose-stone lg:prose-lg mx-auto">
-          {/* <h1>{post.title}</h1> */}
+            {/* <h1>{post.title}</h1> */}
             {/* {matterResult.content} */}
             {/* <Markdown>{matterResult.content}</Markdown> */}
             {parse(post.contents)}
           </div>
         </>
-      ))}
+      )}
     </>
   );
 }
--- components/post/list/PostList.tsx
+++ components/post/list/PostList.tsx
@@ -1,12 +1,31 @@
-"use client";
+// "use client";
 
 import { useEffect, useState } from "react";
 import PostPreview from "../preview/PostPreview";
 import Pagination from "./Pagination";
 import Spinner from "@/components/spinner/Spinner";
 
+
+const getData = async (id: string) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_HOST}/api/post/${id}`,
+    // `/api/post/${id}`,
+    {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      // cache: process.env.NODE_ENV !== "development" && "default" || "no-cache"
+      cache: "no-cache",
+      next: { revalidate: 10 },
+    }
+  );
+  // console.log(await res.json());
+  return res.json();
+};
 const getPostList = async (page: number = 0, limit: number = 10) => {
-  const res = await fetch(`/api/post/list`, {
+  const res = await fetch(
+  // `/api/post/list`, 
+  `${process.env.NEXT_PUBLIC_API_HOST}/api/post/list`,
+  {
     method: "POST",
     body: JSON.stringify({
       page: page,
@@ -23,34 +42,35 @@ const getPostList = async (page: number = 0, limit: number = 10) => {
     }),
     headers: { "Content-Type": "application/json" },
     // cache: process.env.NODE_ENV !== "development" && "default" || "no-cache",
+    cache: 'no-cache',
     next: { revalidate: 60 },
   });
   // console.log(await res.json());
   return res.json();
 };
-export default function PostList({ pageIndex }: { pageIndex: string }) {
-  // const { posts, pages } = await getPostList(Number(pageIndex));
-  const [posts, setPosts] = useState(null as unknown as Array<any>);
-  const [pages, setPages] = useState(null as unknown as {});
-  const [isLoading, setIsLoading] = useState(false);
-  useEffect(() => {
-    setIsLoading(true);
-    getPostList(Number(pageIndex))
-      .then((data) => {
-        console.log(data);
-        // const [posts, pages] = data;
-        if (data.posts && data.pages) {
-          setPosts(data.posts);
-          setPages(data.pages);
-        }
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, [pageIndex]);
+export default async function PostList({ pageIndex }: { pageIndex: string }) {
+  const { posts, pages } = await getPostList(Number(pageIndex));
+  // const [posts, setPosts] = useState(null as unknown as Array<any>);
+  // const [pages, setPages] = useState(null as unknown as {});
+  // const [isLoading, setIsLoading] = useState(false);
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   getPostList(Number(pageIndex))
+  //     .then((data) => {
+  //       console.log(data);
+  //       // const [posts, pages] = data;
+  //       if (data.posts && data.pages) {
+  //         setPosts(data.posts);
+  //         setPages(data.pages);
+  //       }
+  //     })
+  //     .finally(() => {
+  //       setIsLoading(false);
+  //     });
+  // }, [pageIndex]);
   return (
     <>
-      {isLoading && <Spinner />}
+      {/* {isLoading && <Spinner />} */}
       {posts &&
         (posts as Array<any>).map((post) => (
           <PostPreview
