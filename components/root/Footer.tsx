7812683172f9f6c69b85ed34c81a7dc08fa67fa1
Kwon KiHoon <inyourcareallen@gmail.com>
2023-07-17 12:43:40 +0900

    푸터 변경

--- components/root/Footer.tsx
+++ components/root/Footer.tsx
@@ -25,7 +25,7 @@ async function Footer() {
       <div className="w-full flex justify-center items-center bg-gray-100 h-[50px] border-y-2 border-gray-200 text-sm mt-[100px]">
         <p>{`COPYRIGHT (C) ${businessInfo?.name}. ALL RIGHTS RESERVED.`}</p>
       </div>
-      <div className="w-full flex flex-col py-[20px] prose mx-auto text-sm ">
+      <div className="w-full flex flex-col py-[20px] prose mx-auto text-xs justify-center items-center md:justify-start md:items-start md:text-sm">
         <p className="my-1">
           <strong>업체명</strong> {businessInfo?.name} | <strong>대표자</strong>{" "}
           {businessInfo?.bossName} | <strong>사업자등록번호</strong>{" "}
