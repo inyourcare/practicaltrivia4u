cdda2be1f031359063e9641da2845b52625c0e63
Kwon KiHoon <inyourcareallen@gmail.com>
2023-07-20 15:20:25 +0900

    router push 다음번에 테스트 해  보자

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
 "use client";
 import Header from "@/components/root/Header";
 import Link from "next/link";
-import { redirect } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
   // redirect('/edu4u')
   const linkId = "main-page-link-id"
+  const router = useRouter()
   useEffect(() => {
-    const link = document.getElementById(
-      linkId
-    ) as HTMLAnchorElement;
-    link.click();
-  }, []);
+    // const link = document.getElementById(
+    //   linkId
+    // ) as HTMLAnchorElement;
+    // link.click();
+    router.push('/edu4u')
+  }, [router]);
   return (
     <main>
       <header className="w-full flex flex-col py-5 bg-[rgba(35,46,82,0)]">
