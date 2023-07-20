ab96533bed799aae95806e1db483da4cb3154c58
Kwon KiHoon <inyourcareallen@gmail.com>
2023-07-20 13:22:54 +0900

    banner 이미지 넣고 신청하기 버튼 배경 색 변경

--- components/ask/Form.tsx
+++ components/ask/Form.tsx
@@ -360,7 +360,7 @@ export default function Form({
           <button
             className={
               // "absolute bottom-2 right-10 rounded-xl bg-gray-300 p-2 min-w-[90px] justify-center items-center border text-xs font-bold " +
-              " rounded-xl bg-gray-300 p-2 min-w-[90px] justify-center items-center border text-xs font-bold " +
+              " rounded-xl bg-emerald-100 border-emerald-400 p-2 min-w-[90px] justify-center items-center border text-xs font-bold " +
               "max-h-[80px] h-10"
               // (isProcessing ? "disabled" : "")
             }
@@ -374,7 +374,7 @@ export default function Form({
             <div
               className={
                 // "absolute bottom-2 right-10 rounded-xl bg-gray-300 p-2 min-w-[90px] justify-center items-center border text-xs font-bold " +
-                " rounded-xl bg-rose-500 p-2 min-w-[90px] justify-center items-center border text-xs font-bold " +
+                " rounded-xl bg-rose-500 border-rose-800 p-2 min-w-[90px] justify-center items-center border text-xs font-bold " +
                 "max-h-[80px] h-10 flex justify-center items-center"
                 // (isProcessing ? "disabled" : "")
               }
--- components/ask/booking/form/BookingMainForm.tsx
+++ components/ask/booking/form/BookingMainForm.tsx
@@ -119,7 +119,7 @@ export default function BookingMainForm({
           height={250}
           style={{ maxHeight: "250px" }}
           className="rounded-md"
-          src={`/images/ask/booking/banner.png`}
+          src={`/images/ask/booking/banner-yellow.png`}
           alt=""
         />
       </div>
@@ -262,7 +262,7 @@ export default function BookingMainForm({
             <button
               className={
                 // "absolute bottom-2 right-10 rounded-xl bg-gray-300 p-2 min-w-[90px] justify-center items-center border text-xs font-bold " +
-                " rounded-xl bg-gray-300 p-2 min-w-[90px] justify-center items-center border text-xs font-bold " +
+                " rounded-xl bg-yellow-200 p-2 min-w-[90px] justify-center items-center border text-xs font-bold " +
                 "max-h-[80px] h-10"
                 // (isProcessing ? "disabled" : "")
               }
