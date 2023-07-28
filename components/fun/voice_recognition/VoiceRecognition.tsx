280cd2d482f305a604aca95b2f72d5a99ce7c4de
Kwon KiHoon <inyourcareallen@gmail.com>
2023-07-28 21:15:31 +0900

    필터버튼 추가

--- components/fun/voice_recognition/VoiceRecognition.tsx
+++ components/fun/voice_recognition/VoiceRecognition.tsx
@@ -1,81 +1,114 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { Word } from "@prisma/client";
+import { levels } from "./levels";
+import DeviceControl from "./DeviceControl";
 
-export default function VoiceRecognition({ words }: { words: any }) {
+export default function VoiceRecognition({ words }: { words: Word[] }) {
   const [spoken, setSpoken] = useState("");
+  const [filteredLevels, setFilteredLevels] = useState(new Set<string>());
+  const [filteredWords, setFilteredWords] = useState(null as unknown as Word[]);
   function startAndRefreshSpeechRecognition() {
-    const SpeechRecognition =
-      window.SpeechRecognition || window.webkitSpeechRecognition;
-    const recognition = new SpeechRecognition();
-    // true면 음절을 연속적으로 인식하나 false면 한 음절만 기록함
-    recognition.interimResults = true;
-    // 값이 없으면 HTML의 <html lang="en">을 참고합니다. ko-KR, en-US
-    // recognition.lang = "ko-KR";
-    recognition.lang = "en-US";
-    // true means continuous, and false means not continuous (single result each time.)
-    // true면 음성 인식이 안 끝나고 계속 됩니다.
-    recognition.continuous = true;
-    // 숫자가 작을수록 발음대로 적고, 크면 문장의 적합도에 따라 알맞은 단어로 대체합니다.
-    // maxAlternatives가 크면 이상한 단어도 문장에 적합하게 알아서 수정합니다.
-    recognition.maxAlternatives = 10000;
-    // setSpeechRecognition(recognition);
+    if (
+      window &&
+      (window.SpeechRecognition || window.webkitSpeechRecognition)
+    ) {
+      const SpeechRecognition =
+        window.SpeechRecognition || window.webkitSpeechRecognition;
+      const recognition = new SpeechRecognition();
+      // true면 음절을 연속적으로 인식하나 false면 한 음절만 기록함
+      recognition.interimResults = true;
+      // 값이 없으면 HTML의 <html lang="en">을 참고합니다. ko-KR, en-US
+      // recognition.lang = "ko-KR";
+      recognition.lang = "en-US";
+      // true means continuous, and false means not continuous (single result each time.)
+      // true면 음성 인식이 안 끝나고 계속 됩니다.
+      recognition.continuous = true;
+      // 숫자가 작을수록 발음대로 적고, 크면 문장의 적합도에 따라 알맞은 단어로 대체합니다.
+      // maxAlternatives가 크면 이상한 단어도 문장에 적합하게 알아서 수정합니다.
+      recognition.maxAlternatives = 10000;
+      // setSpeechRecognition(recognition);
 
-    let speechToText = "";
-    recognition.addEventListener("result", (e: any) => {
-      let interimTranscript = "";
-      console.log("recognition result");
-      for (let i = e.resultIndex, len = e.results.length; i < len; i++) {
-        let transcript = e.results[i][0].transcript;
-        const isFinal = e.results[i].isFinal;
-        if (e.results[i].isFinal) {
-          // console.log(transcript);
-          setSpoken(transcript);
+      let speechToText = "";
+      recognition.addEventListener("result", (e: any) => {
+        let interimTranscript = "";
+        console.log("recognition result");
+        for (let i = e.resultIndex, len = e.results.length; i < len; i++) {
+          let transcript = e.results[i][0].transcript;
+          const isFinal = e.results[i].isFinal;
+          if (e.results[i].isFinal) {
+            // console.log(transcript);
+            setSpoken(transcript);
+          }
         }
-      }
-    });
+      });
 
-    // 음성인식이 끝나면 자동으로 재시작합니다.
-    // recognition.addEventListener("end", recognition.start);
+      // 음성인식이 끝나면 자동으로 재시작합니다.
+      // recognition.addEventListener("end", recognition.start);
 
-    // 음성 인식 시작
-    recognition.start();
-    console.log("speech recognition starts");
+      // 음성 인식 시작
+      recognition.start();
+      console.log("speech recognition starts");
+    }
   }
-  startAndRefreshSpeechRecognition();
+  async function getMedia(constraints: MediaStreamConstraints) {
+    let stream = null;
+
+    try {
+      stream = await navigator.mediaDevices.getUserMedia(constraints);
+      /* 스트림 사용 */
+    } catch (err) {
+      /* 오류 처리 */
+      console.error("error while getMedia", err);
+    }
+  }
   useEffect(() => {
-    // let dbWords = new Array<any>();
-    // (words as Array<any>).map((fileWord) => {
-    //   const fileName = fileWord[0] as string;
-    //   const content = (fileWord[1] as string)
-    //     .replace(/(?:\t)/g, " ")
-    //     .replace(/(?:\r\n|\r|\n)/g, "<br />")
-    //     .split("<br />")
-    //     .map((s) => {
-    //       const splits = s.trim().split(" ");
-    //       const korean = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
-    //       let i = 0;
-    //       for (; i < splits.length; i += 1)
-    //         if (korean.test(splits[i]) === true) break;
-    //       return {
-    //         spell: splits.slice(0, i).join(" "),
-    //         korean: splits.slice(i).join(" "),
-    //         level: fileName.split('.')[0]
-    //       };
-    //     });
-    //   console.log(fileName, content);
-    //   dbWords = dbWords.concat(content)
-    // });
-    // console.log('dbWords',dbWords)
-    console.log(words)
+    startAndRefreshSpeechRecognition();
+    
+    // console.log(words)
+    console.log();
   }, [words]);
+  useEffect(() => {
+    // console.log(
+    //   "filtered words",
+    //   words.filter((word) => filteredLevels.has(word.level))
+    // );
+    setFilteredWords(words.filter((word) => filteredLevels.has(word.level)));
+  }, [filteredLevels, words]);
   return (
     <div className="w-full flex justify-center items-center flex-col">
-      <button onClick={() => startAndRefreshSpeechRecognition()}>
-        refresh
-      </button>
+      <div className="flex items-center justify-center flex-col">
+        {/* <DeviceControl/> */}
+        ※마이크 변경(크롬) chrome://settings/content/microphone
+        {/* <a href="https://www.freecodecamp.org/" target="_blank"></a> */}
+        <button onClick={() => startAndRefreshSpeechRecognition()}>
+          마이크 새로고침 (아이콘)
+        </button>
+      </div>
       <br />
       {spoken}
+      <br />
+      <div className="flex items-center justify-center">
+        <div className="w-full flex flex-raw ">
+          {Object.keys(levels).map((key) => (
+            <button
+              className={`border ml-1 ${
+                filteredLevels.has(key) ? "bg-blue-500" : "bg-blue-200"
+              } hover:border hover:border-blue-500 text-white font-bold px-4 rounded disabled:cursor-not-allowed`}
+              key={key}
+              onClick={() => {
+                filteredLevels.has(key)
+                  ? filteredLevels.delete(key)
+                  : filteredLevels.add(key);
+                setFilteredLevels(new Set(filteredLevels));
+              }}
+            >
+              {key}
+            </button>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ components/fun/voice_recognition/levels.ts
@@ -0,0 +1,6 @@
+export const levels = {
+  elementary:'elementary',
+  high:'high',
+  middle: 'middle',
+  toeic: 'toeic'
+}
