19b322ee009d1e771b4fc5e81972239919695a5b
Kwon KiHoon <inyourcareallen@gmail.com>
2023-07-30 17:51:16 +0900

    파일이름 포맷 변경

--- components/fun/voice_recognition/screenshot.ts
+++ components/fun/voice_recognition/screenshot.ts
@@ -1,12 +1,25 @@
 import html2canvas from "html2canvas";
 
+function leftPad(value:number) {
+  if (value >= 10) {
+      return value;
+  }
+
+  return `0${value}`;
+}
+
 export function clickToScreenShot(elemId: string, filename: string) {
   const elem = document.getElementById(elemId);
   if (elem) {
+    const date = new Date()
+    const year = date.getFullYear();
+    const month = leftPad(date.getMonth() + 1);
+    const day = leftPad(date.getDate());
+    const filedate = [year, month, day].join('_');
     html2canvas(elem, {}).then(function (canvas) {
       saveAs(
-        canvas.toDataURL(),
-        `${new Date().toLocaleString()}_${filename}.png`
+        canvas.toDataURL("image/png", 1.0),
+        `${filedate}_${filename}.png`
       );
     });
   }
