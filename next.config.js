4537a724d6ff8b409c7afdacf4fe812105ac3b08
inyourcare <inyourcareallen@gmail.com>
2023-08-02 10:00:21 +0900

    redirect 추가

--- next.config.js
+++ next.config.js
@@ -6,6 +6,42 @@ const nextConfig = {
         source: '/',
         destination: '/edu4u',
         permanent: false,
+      },{
+        source: '/intro/wawa/:path*',
+        destination: '/partners/wawa',
+        permanent: false,
+      },{
+        source: '/intro/goodo',
+        destination: '/partners/goodo',
+        permanent: false,
+      },{
+        source: '/post/about.md',
+        destination: '/edu4u',
+        permanent: false,
+      },{
+        source: '/post/10_howcoding',
+        destination: '/post/detail/10',
+        permanent: false,
+      },{
+        source: '/post/11_solute_4.md',
+        destination: '/post/detail/11',
+        permanent: false,
+      },{
+        source: '/post/7_goodo.md',
+        destination: '/post/detail/7',
+        permanent: false,
+      },{
+        source: '/post/13_wawa_230106',
+        destination: '/post/detail/13',
+        permanent: false,
+      },{
+        source: '/post/11_solute_4',
+        destination: '/post/detail/11',
+        permanent: false,
+      },{
+        source: '/post/4_solute',
+        destination: '/post/detail/4',
+        permanent: false,
       },
     ]
   },
